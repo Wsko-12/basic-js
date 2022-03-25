29d1e9981323975a10fd461f1531a9381632a45c
Wsko-12 <vaskovladislav@gmail.com>
2022-03-25 18:15:06 +0300

    feat: add solution to vigenere-cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,78 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(direct){
+    this.reverse = direct === false ? true: false;
+
+    this.alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    this.table = [];
+
+    for(let i=0;i<this.alphabet.length;i++){
+      this.table[i] = (this.alphabet.substring(i) + this.alphabet.substring(0,i)).split('');
+    };
+
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  encrypt(str, key) {
+    if(!str || !key){
+      throw new Error("Incorrect arguments!");
+    }
+    str = str.toUpperCase();
+    key = key.toUpperCase();
+    
+    let crypted = '';
+    let letterShift = 0; 
+    str.split('').forEach((letter, i) => {
+      const x = this.alphabet.indexOf(letter);
+      if(~x){
+        const y = this.alphabet.indexOf(key.charAt(letterShift));
+        letterShift++;
+        if(letterShift >= key.length){
+          letterShift = 0;
+        };
+        crypted+= this.table[x][y];
+      }else{
+        crypted+=letter;
+      };
+    });
+
+    if(this.reverse){
+      crypted = crypted.split('').reverse().join('');
+    };
+    return crypted;
+  }
+  decrypt(str, key) {
+    if(!str || !key){
+      throw new Error("Incorrect arguments!");
+    }
+
+    key = key.toUpperCase();
+
+    let decrypted = '';
+    let letterShift = 0; 
+    
+    str.split('').forEach((letter)=>{
+      const isLetter = this.alphabet.indexOf(letter);
+      if(~isLetter){
+        const xIndex = this.alphabet.indexOf(key.charAt(letterShift));
+        for(let i=0; i<this.alphabet.length;i++){
+          if(this.table[i][xIndex] === letter){
+            decrypted += this.alphabet.charAt(i);
+          };
+        };
+        letterShift++;
+        if(letterShift >= key.length){
+          letterShift = 0;
+        };
+      }else{
+        decrypted+=letter;
+      };
+    });
+
+    if(this.reverse){
+      decrypted = decrypted.split('').reverse().join('');
+    };
+
+    return decrypted;
   }
 }
 
