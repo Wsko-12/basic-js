c7857d7de3e1038c3c168e0baaadf250ade33d1c
Wsko-12 <vaskovladislav@gmail.com>
2022-03-31 13:47:52 +0300

    feat: add solution to dns-stats

--- src/dns-stats.js
+++ src/dns-stats.js
@@ -22,9 +22,21 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getDNSStats(domains) {
+    const subDomains = {};
+    domains.forEach(item =>{
+      const arr = item.split('.');
+      let domain = '';
+      for(let i = arr.length-1;i>=0;i--){
+          domain += `.${arr[i]}`;
+          if(subDomains[domain]){
+              subDomains[domain]++
+          }else{
+              subDomains[domain] = 1;
+          };
+      };
+    });
+    return subDomains;
 }
 
 module.exports = {
